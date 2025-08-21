6efd300dafe230047c41d325d8abfbe729b5fa27
vitorSiquera <vitor.siqueira.simeao@gmail.com>
2025-08-21 14:39:24 -0300

    (LoginController):implement login, logout and error feedback in authentication flow

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,7 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\LoginRequest;
-
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -15,6 +16,25 @@ class LoginController extends Controller
     public function loginProcess(LoginRequest $request)
     {
        $request->validated();
-          
+
+       $authenticated = Auth::attempt([
+            'email' => $request->email,
+            'password' => $request->password,
+        ]);
+
+        if(!$authenticated) {
+            return redirect()->back()->withErrors(['email' => 'Email ou Senha incorretos'])->withInput();
+        }        
+        
+        $user = Auth::user();
+        $user = User::find($user->id);
+
+        return redirect()->route('user.index')->with('success', 'Login successful!');
+    }
+
+    public function destroy()
+    {
+        Auth::logout();
+        return redirect()->route('login')->with('success', 'Perfil deslogado');
     }
 }
